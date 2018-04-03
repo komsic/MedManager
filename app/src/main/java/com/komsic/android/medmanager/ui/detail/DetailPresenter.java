14113be46ddd3a19cd5133e39b418912e0c49e94
komsic <kayodemicheal159@gmail.com>
2018-04-03 21:58:59 +0100

    add travis

--- app/src/main/java/com/komsic/android/medmanager/ui/detail/DetailPresenter.java
+++ app/src/main/java/com/komsic/android/medmanager/ui/detail/DetailPresenter.java
@@ -6,13 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.komsic.android.medmanager.data.DataManager;
 import com.komsic.android.medmanager.data.model.Med;
 import com.komsic.android.medmanager.data.model.Reminder;
